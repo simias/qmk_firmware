cac03cfe365441dc0519a1b87974a6ded160db11
Lionel Flandrin <lflandrin@ereca.fr>
2018-08-30 12:03:33 +0200

    Bind Application/Menu key

--- keyboards/ergodox_ez/keymaps/simias/keymap.c
+++ keyboards/ergodox_ez/keymaps/simias/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_F12,         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRNS,
 	KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_UP,          LSFT(KC_INSERT),KC_TRNS,
 	KC_TRNS,        KC_HOME,        KC_TRNS,        KC_END,         KC_TRNS,        KC_TRNS,
-	KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+	KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_APP,
 	KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
 	                                                                                KC_TRNS,        KC_TRNS,
 	                                                                                                KC_TRNS,
