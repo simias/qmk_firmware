3a1ddcee24199ab7c87c17803460852aee7c9c0b
Lionel Flandrin <lfg@svkt.eu>
2020-09-24 21:34:24 +0100

    Moonlander: remap page up and down

--- keyboards/moonlander/keymaps/simias/keymap.c
+++ keyboards/moonlander/keymaps/simias/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TRNS,           KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         CT_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_APP,            KC_ENT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT,              KC_ESC,   KC_MUTE,               KC_MINS, KC_EQL,  KC_TRNS, KC_TRNS, TO(3),
+        KC_PGDN, KC_PGUP, KC_TRNS, KC_TRNS, KC_LALT,              KC_ESC,   KC_MUTE,               KC_MINS, KC_EQL,  KC_TRNS, KC_TRNS, TO(3),
                                             KC_SPC,  MO(2),   KC_LGUI,           KC_TRNS, MO(1),   KC_RALT
     ),
 
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12,  KC_F7,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,           KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS,           KC_TRNS, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_HOME, KC_TRNS, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_DEL,  KC_BSPC, KC_TRNS, KC_DOWN, KC_RBRC, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_PGUP, KC_TRNS,                             KC_LEFT, KC_ENT,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS,                             KC_LEFT, KC_ENT,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,   KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS
     ),
