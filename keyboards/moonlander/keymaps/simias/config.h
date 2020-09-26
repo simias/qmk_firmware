cef2aa6b16ff4c35fe5e72e16c2f179e44ed18ce
Lionel Flandrin <lfg@svkt.eu>
2020-09-26 01:16:50 +0100

    Moonlander: add GB-style boot sound

--- keyboards/moonlander/keymaps/simias/config.h
+++ keyboards/moonlander/keymaps/simias/config.h
@@ -2,5 +2,11 @@
 
 #define PERMISSIVE_HOLD
 
+#define GB_BOOTROM H__NOTE(_C7), M__NOTE(_C8, 160)
+
+#ifdef AUDIO_ENABLE
+# define STARTUP_SONG SONG(GB_BOOTROM)
+#endif
+
 /* This breaks the scroll lock toggle shenanigans */
 #undef ORYX_CONFIGURATOR
